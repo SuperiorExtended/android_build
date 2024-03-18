ae6b91f8fb1a1fd9e0a5ed1889533945bc1986ac
Matt Filetto <matt.filetto@gmail.com>
2024-03-18 10:40:30 -0700

    target: Exclude Sample APN when compiling a superior based build.
    * GSI Targets like husky fail without this. Inspired by LOS
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -27,5 +27,7 @@ PRODUCT_PACKAGES += \
 
 # Telephony:
 #   Provide a APN configuration to GSI product
+ifeq ($(SUPERIOR_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif
